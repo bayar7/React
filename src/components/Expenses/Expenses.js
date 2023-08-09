d655c5e2c2882ae4f31573e1b79a5a3b6ce71581
ドルジダグワバヤル <hj8457@Bayar.elecom>
2023-08-09 19:41:10 +0900

    fix: Refactored filtering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,9 +6,36 @@ import { useState } from "react";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2021");
+
+  let filterInfoText = "2019, 2021 & 2022";
+
+  if (filteredYear == "2019") {
+    filterInfoText = "2020, 2021 & 2022";
+  } else if (filteredYear == "2020") {
+    filterInfoText = "2019, 2021 & 2022";
+  } else {
+    filterInfoText = "2019, 2020 & 2021";
+  }
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -16,26 +43,8 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {expensesContent}
+        <p>Data for years {filterInfoText} is hidden.</p>
       </Card>
     </div>
   );
